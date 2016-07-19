63b03b4f0eec563d85c0dc04b55e5997b79cc628
Simon Fojtu <simon.fojtu@gmail.com>
2016-07-19 20:23:37 +0200

    receiver firmware

---
+++ receiver/firmware/src/main.cpp
@@ -0,0 +1,144 @@
+/*
+ * @Author  : Simon Fojtu
+ * @Date    : 16.07.2016
+ */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <util/atomic.h>
+
+
+/* Global variables */
+/* time since startup in 1/TIMER0_OVF_FREQ s */
+volatile long mtime;
+/* device status */
+//status_t stat;
+
+static char mtc;
+
+//!
+// Drive DC motor using two signals
+//
+// e.g. design 1 from http://www.talkingelectronics.com/projects/H-Bridge/H-Bridge-1.html
+//
+// The class ensures that only one of the pins is HIGH at any time (to avoid shoot through)
+// in the H-bridge.
+//
+class MotorDriver {
+    public:
+        enum Status {
+            stopped,
+            forward,
+            reverse
+        };
+
+        MotorDriver(volatile uint8_t* out, volatile uint8_t* ddr, uint8_t pinA, uint8_t pinB) {
+            _OUT = out;
+            _PINA = pinA;
+            _PINB = pinB;
+
+            *_OUT &= !(1 << _PINA | 1 << _PINB);
+            *ddr |= (1 << _PINA) | (1 << _PINB);
+
+            _status = stopped;
+        }
+
+        void fwd() {
+            if (_status == reverse) {
+               *_OUT &= ~(1 << _PINB);
+               _delay_ms(10);
+            }
+            *_OUT |= (1 << _PINA);
+            _status = forward;
+        }
+
+        void rev() {
+            if (_status == forward) {
+               *_OUT &= ~(1 << _PINA);
+               _delay_ms(10);
+            }
+            *_OUT |= (1 << _PINB);
+            _status = reverse;
+        }
+
+        void stop() {
+            *_OUT &= ~((1 << _PINA) | (1 << _PINB));
+            _status = stopped;
+        }
+
+    private:
+        Status _status;
+        uint8_t _PINA, _PINB;
+        volatile uint8_t* _OUT;
+};
+
+MotorDriver motorDriver(&PORTB, &DDRB, PB3, PB4);
+
+/*!
+ * Timer0 overflow interrupt handling routine
+ */
+ISR(TIMER0_OVF_vect)
+{
+        /* frequency TIMER0_OVF_FREQ Hz */
+        mtime++;
+}
+
+
+void main_init(void)
+{
+//        spi_init();
+//
+//        hw_init();
+//
+//        timer0_init();
+
+        mtime = 0;
+        mtc = 0;
+}
+
+void main_tick(void)
+{
+        /* Update time */
+//        ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+//                stat.ltime = mtime;
+//        }
+//        stat.sec = stat.ltime / TIMER0_OVF_FREQ;
+
+        /* Process buttons */
+//        Keyboard_tick(&stat);
+
+        /* Slow tick */
+        if (mtc == 255) {
+//                hw_read_tick(&stat);
+//
+//                hw_write_tick(&stat);
+        }
+
+        mtc++;
+}
+
+int main(void)
+{
+
+        main_init();
+
+//        timer0_start();
+
+        sei();
+
+
+        while (1) {
+            motorDriver.fwd();
+            _delay_ms(100);
+            motorDriver.stop();
+            _delay_ms(100);
+            motorDriver.rev();
+            _delay_ms(100);
+            motorDriver.stop();
+            _delay_ms(100);
+        }
+
+        return(0);
+}
+
