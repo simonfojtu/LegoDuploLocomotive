56103673749b0b26888b7462a828996ff835f274
Simon Fojtu <simon.fojtu@gmail.com>
2016-07-20 20:43:28 +0200

    fixed interrupts and sleep mode; wdt timeout extended to 120s

--- receiver/firmware/src/main.cpp
+++ receiver/firmware/src/main.cpp
@@ -5,6 +5,7 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/sleep.h>
 #include <avr/wdt.h>
 #include <util/delay.h>
 #include <util/atomic.h>
@@ -14,22 +15,27 @@
 
 MotorDriver motorDriver(&PORTB, &DDRB, PB3, PB4);
 
+// set to 1 if button at PB2 is pressed long enough
 volatile int longpress = 0;
+
 volatile uint8_t timer0_counter = 0;
 ISR(TIMER0_OVF_vect)
 {
     timer0_counter++;
-    if (timer0_counter == 8) {
+    if (timer0_counter == 5) {
         longpress = 1;
         timer0_counter = 0;
         TIMSK &= ~(1 << TOIE0); // disable Timer/Counter0 overflow interrupt
     }
 }
 
-volatile int PB2_state; // state of PB2 when INT0 interrupt fires
+// state of PB2 when INT0 interrupt fires
+volatile int PB2_state;
+
+// motor driver status
 volatile MotorDriver::Status motorStatus = motorDriver.getStatus();
 
-ISR(INT0_vect) {
+ISR(PCINT0_vect) {
     PB2_state = PINB & (1 << PB2);
 
     if (PB2_state) { // button pressed
@@ -38,7 +44,8 @@ ISR(INT0_vect) {
         longpress = 0;
         TCNT0 = 0; // reset Timer/Counter0
         TIMSK |= 1 << TOIE0; // enable Timer/Counter0 overflow interrupt
-        // TODO start timer to measure how long it is pressed
+        // set idle sleep mode, where the timer/counter0 is still running
+        MCUCR &= ~(1 << SM1 | 2 << SM0); // Idle sleep mode
     } else { // button released
         if (motorStatus == MotorDriver::stopped) {
             if (longpress == 1)
@@ -46,22 +53,29 @@ ISR(INT0_vect) {
             else
                 motorDriver.fwd();
         }
+        // set power down sleep mode, where only pcint0 and watchdog can wake us
+        MCUCR |= (1 << SM1); // Power-down sleep mode
     }
+    // keep watchdog happy
     wdt_reset();
 }
 
+volatile uint8_t watchdog_counter = 0;
 ISR(WDT_vect) {
-    // Watchdog fired
-    motorDriver.stop();
+    watchdog_counter++;
+    // Watchdog fires every 8 seconds.
+    // 8 * 15 = 120s, thus after 2 minutes the motor is stopped.
+    if (watchdog_counter == 15) {
+        motorDriver.stop();
+        watchdog_counter = 0;
+    }
 }
 
-
-void main_init(void)
+int main(void)
 {
-    // set up external interrupt on PB2 - INT0
-    //MCUCR |= 1 << ISC01; // The falling edge of INT0 generates an interrupt request [p51]
-    MCUCR |= 1 << ISC00; // Any logical change on INT0 generates an interrupt request [p51]
-    GIMSK |= 1 << INT0; // Enable external interrupt request 0
+    // set up external interrupt on PB2 - PCINT2
+    PCMSK |= 1 << PCINT2; // Pin change enable mask
+    GIMSK |= 1 << PCIE; // Pin change interrupt enable
 
     // set up watchdog
     WDTCR |= 1 << WDIE; // enable watchdog interrupt
@@ -70,20 +84,13 @@ void main_init(void)
     // set up button press timer
     TCCR0B |= (1 << CS02) | (1 << CS00); // prescaler 1/1024 -> overflow 3.81Hz at 1MHz clock
 
-}
+    sei(); // enable interrupts
 
+    while (1) {
+        MCUCR |= (1 << SE); // sleep enable
+        sleep_mode(); //Go to sleep
+    }
 
-int main(void)
-{
-
-        main_init();
-
-        sei();
-
-        while (1) {
-            // sleep();
-        }
-
-        return(0);
+    return(0);
 }
 
